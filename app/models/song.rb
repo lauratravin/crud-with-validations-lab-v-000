efeb82eb5f02d7e65350d697bc212d28d10933ef
Laura Messa Travin <laura.travin@gmail.com>
2021-03-21 19:37:05 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -25,10 +25,10 @@
 
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqueness: {
-    scope: %i[release_year artist_name],
-    message: 'cannot be repeated by the same artist in the same year'
-  }
+  # validates :title, uniqueness: {
+  #   scope: %i[release_year artist_name],
+  #   message: 'cannot be repeated by the same artist in the same year'
+  # }
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
 
